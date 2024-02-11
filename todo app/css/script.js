94940c4e10266be2d208a3e0c29b50a400b7e182
SAKSHIPARGE <159707891+SAKSHIPARGE@users.noreply.github.com>
2024-02-11 20:31:17 +0530

    Add files via upload

---
+++ todo app/css/script.js
@@ -0,0 +1,12 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask() {
+    if (inputBox.value === '') {
+        alert("you must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer, appendChild(li);
+    }
+}
